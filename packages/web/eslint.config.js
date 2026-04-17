ac692ee003401d28d6cf41385783eb3a8aec8058
Kia Abdi <65511468+TheeKia@users.noreply.github.com>
2026-04-17 05:03:57 +0300

    init

---
+++ packages/web/eslint.config.js
@@ -0,0 +1,30 @@
+import js from "@eslint/js";
+import pluginQuery from "@tanstack/eslint-plugin-query";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+export default defineConfig([
+  globalIgnores(["dist"]),
+  {
+    files: ["**/*.{ts,tsx}"],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat["recommended-latest"],
+      reactRefresh.configs.vite,
+      pluginQuery.configs["flat/recommended"],
+    ],
+    languageOptions: {
+      ecmaVersion: "latest",
+      globals: globals.browser,
+    },
+    rules: {
+      "react-refresh/only-export-components": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+    },
+  },
+]);
