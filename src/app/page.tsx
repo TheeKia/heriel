ddd0fa0a7a03158a337edf60c9048d9a6becf11f
Kia <thekiabdi@gmail.com>
2026-09-05 19:56:11 +0300

    fresh start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
 
   return (
     <main className="relative flex min-h-dvh flex-col items-center justify-center gap-4 overflow-hidden">
-      <Dotimation item={ITEM} width={128} height={128} pointSpacingCss={1} />
+      <Dotimation item={ITEM} width={128} height={128} dots={{ size: "hairline" }} />
 
       <Textimation
         text={CYCLE_MESSAGE[messageIdx]}
